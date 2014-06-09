9a6359b8149ad85cb3d7641610a8249f6c7b8f9c
masaru0925 <masaru.0925@gmail.com>
2014-06-09 23:18:44 +0900

    linux

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LIB=/var/local/lib/redis
+LOG=/var/local/log/redis
+RUN=/var/local/run/redis
+ETC=/etc/redis
+
+for TARGET in {$LIB, $LOG, $RUN, $ETC}; do
+	mkdir -p $TARGET
+	chown -R redis.redis $TARGET
+done
+ETC=/etc/haproxy
+mkdir -p $ETC
+chown -R haproxy.haproxy $ETC
